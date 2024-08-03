7a71876628a2215bb5d2b95507665bcefc9cae60
jjoaquiimsx <mf.joaquim2005@gmail.com>
2024-08-03 08:45:39 -0300

    Logo e nome do site

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <Container>
-        <Top>Catalogo Kaliffon</Top>
+        <Top>Catalogo Kallifon</Top>
         <Main />
         <Footer />
       </Container>
